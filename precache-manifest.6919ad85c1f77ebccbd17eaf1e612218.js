0e2ca1b209d67d9f1b0934805eaa7971ad349a05
122208997 <lehaohust@gmail.com>
2020-02-17 13:48:46 +0700

    Updates

---
+++ precache-manifest.6919ad85c1f77ebccbd17eaf1e612218.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21be82467bd0eb90b00dd3bea16aae75",
+    "url": "/world-news/index.html"
+  },
+  {
+    "revision": "59f18e99dd0e4cb62221",
+    "url": "/world-news/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ad074ef6984e3013841f",
+    "url": "/world-news/static/css/main.373c1cc9.chunk.css"
+  },
+  {
+    "revision": "59f18e99dd0e4cb62221",
+    "url": "/world-news/static/js/2.3fc2c91b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/world-news/static/js/2.3fc2c91b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad074ef6984e3013841f",
+    "url": "/world-news/static/js/main.089ea7ab.chunk.js"
+  },
+  {
+    "revision": "01afc45ef8c8a0e6584f",
+    "url": "/world-news/static/js/runtime-main.2dbd54bc.js"
+  },
+  {
+    "revision": "70433cc5b5fb24931b49765a8a9fbd1a",
+    "url": "/world-news/static/media/bird.70433cc5.jpg"
+  },
+  {
+    "revision": "6025a15447e0ff4a18f270bad3fe2977",
+    "url": "/world-news/static/media/bridge.6025a154.jpg"
+  },
+  {
+    "revision": "46128c96031b21add4c62872b7e358d0",
+    "url": "/world-news/static/media/elephen.46128c96.jpg"
+  },
+  {
+    "revision": "00a92914be9958e51201f3810af41a6a",
+    "url": "/world-news/static/media/moun.00a92914.jpg"
+  },
+  {
+    "revision": "2d5808c377465c8bdf99a0dd8850b710",
+    "url": "/world-news/static/media/mountain.2d5808c3.jpg"
+  },
+  {
+    "revision": "73443ce656b6f717b3b7597c6f12d097",
+    "url": "/world-news/static/media/paddy-field.73443ce6.jpg"
+  },
+  {
+    "revision": "adefbb25ee5fd7b294fe1f30d76407c2",
+    "url": "/world-news/static/media/road.adefbb25.jpg"
+  },
+  {
+    "revision": "3a0f0ba5b083981f79c8ffa9bb0d3e9c",
+    "url": "/world-news/static/media/sky.3a0f0ba5.jpg"
+  },
+  {
+    "revision": "7b3ede01726a15c4a12d5d6026616afd",
+    "url": "/world-news/static/media/trees.7b3ede01.jpg"
+  },
+  {
+    "revision": "0a90d869b2b0cbdbfbe7aafd14dec99b",
+    "url": "/world-news/static/media/umbrella.0a90d869.jpg"
+  }
+]);
